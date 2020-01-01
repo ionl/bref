acf808cf6c73bc9c69b4673d1c8a7ac616753bae
ionl <ionl@users.noreply.github.com>
2020-01-01 04:15:32 +0100

    deployed

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
-echo "sss";
+require __DIR__.'/vendor/autoload.php';
+
+lambda(function ($event) {
+    return 'Hello ' . ($event['name'] ?? 'world');
+});
